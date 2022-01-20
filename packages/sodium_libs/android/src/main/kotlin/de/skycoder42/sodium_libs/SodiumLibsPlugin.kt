5ea257741eedfa05cf4b217b8fdb9954917e52fd
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-01-20 06:57:02 +0100

    Fix android plugin

--- packages/sodium_libs/android/src/main/kotlin/de/skycoder42/sodium_libs/SodiumLibsPlugin.kt
+++ packages/sodium_libs/android/src/main/kotlin/de/skycoder42/sodium_libs/SodiumLibsPlugin.kt
@@ -1,5 +1,10 @@
 package de.skycoder42.sodium_libs
 
+import androidx.annotation.NonNull
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 
-class SodiumLibsPlugin: FlutterPlugin {}
+class SodiumLibsPlugin: FlutterPlugin {
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  }
+}
