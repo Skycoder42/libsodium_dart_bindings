86aa9d51d4ef24638b12d30233879317d5cd2f37
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-03-16 15:51:36 +0000

    ........

--- packages/sodium_libs/tool/libsodium/build.dart
+++ packages/sodium_libs/tool/libsodium/build.dart
@@ -13,8 +13,12 @@ Future<void> main(List<String> args) async {
     case CiPlatform.android_x86:
       createArtifact = _createAndroidArtifact;
       break;
-    case CiPlatform.apple:
-      createArtifact = _createAppleArtifact;
+    case CiPlatform.ios:
+    case CiPlatform.ios_simulator_arm64:
+    case CiPlatform.ios_simulator_x86_64:
+    case CiPlatform.macos_arm64:
+    case CiPlatform.macos_x86_64:
+      createArtifact = _createDarwinArtifact;
       break;
     case CiPlatform.windows:
       createArtifact = _createWindowsArtifact;
@@ -50,28 +54,69 @@ Future<void> _createAndroidArtifact(
   await source.rename(target.path);
 }
 
-Future<void> _createAppleArtifact(
+Future<void> _createDarwinArtifact(
   CiPlatform platform,
   Directory extractDir,
   String lastModifiedHeader,
   Directory artifactDir,
 ) async {
   final buildDir = extractDir.subDir('libsodium-stable');
+  final prefixDir = buildDir.subDir('libsodium-${platform.name}');
+
+  final xcodeDir = Directory('xcode-select -p');
+  final baseDir = xcodeDir
+      .subDir('Platforms')
+      .subDir('${platform.sdk}.platform')
+      .subDir('Developer');
+  final binDir = baseDir.subDir('usr').subDir('bin');
+  final sbinDir = baseDir.subDir('usr').subDir('sbin');
+
+  final baseFlagsBuilder = StringBuffer('-arch ${platform.architecture}');
+
+  Directory? sysrootDir;
+  if (platform.hasSysroot) {
+    sysrootDir = baseDir.subDir('SDKs').subDir('${platform.sdk}.sdk');
+    baseFlagsBuilder.write(" -isysroot '${sysrootDir.path}'");
+  }
+
+  if (platform.extraFlags != null) {
+    baseFlagsBuilder.write(' ${platform.extraFlags}');
+  }
+
+  final baseFlags = baseFlagsBuilder.toString();
+  final environment = {
+    'LIBSODIUM_FULL_BUILD': '1',
+    'PATH': '${binDir.path}:${sbinDir.path}:${Platform.environment['PATH']}',
+    if (sysrootDir != null) 'SDK:': sysrootDir.path,
+    'CFLAGS': '-O2 $baseFlags',
+    'LDFLAGS': baseFlags,
+  };
   await run(
-    './dist-build/apple-xcframework.sh',
-    const [],
+    './configure',
+    [
+      '--host=${platform.buildTarget}',
+      '--prefix=${prefixDir.path}',
+    ],
     workingDirectory: buildDir,
-    environment: const {
-      'LIBSODIUM_FULL_BUILD': '1',
-      'MACOS_VERSION_MIN': '10.11',
-      'IOS_SIMULATOR_VERSION_MIN': '9.0',
-      'IOS_VERSION_MIN': '9.0',
-    },
+    environment: environment,
+  );
+  await run(
+    'make',
+    [
+      '-j${Platform.numberOfProcessors}',
+      'install',
+    ],
+    workingDirectory: buildDir,
+    environment: environment,
   );
 
-  final source =
-      buildDir.subDir('libsodium-apple').subDir('Clibsodium.xcframework');
-  final target = artifactDir.subDir('Clibsodium.xcframework');
+  final source = File(
+    await prefixDir
+        .subDir('lib')
+        .subFile('libsodium.dylib')
+        .resolveSymbolicLinks(),
+  );
+  final target = artifactDir.subFile('libsodium.dylib');
   await target.parent.create(recursive: true);
   await source.rename(target.path);
 }
