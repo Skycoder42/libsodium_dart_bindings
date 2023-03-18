974830770593eaa27fb9528ef50c85219c61d450
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-03-18 18:29:32 +0100

    wip

--- packages/sodium_libs/tool/libsodium/platforms/windows_target.dart
+++ packages/sodium_libs/tool/libsodium/platforms/windows_target.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:meta/meta.dart';
 
+import '../../../../../tool/util.dart';
+import '../github/github_logger.dart';
 import 'plugin_target.dart';
 
 @immutable
@@ -18,8 +20,37 @@ class WindowsTarget extends PluginTarget {
   Future<void> build({
     required Directory extractDir,
     required Directory artifactDir,
-  }) {
-    // TODO: implement build
-    throw UnimplementedError();
+  }) async {
+    const arch = 'x64';
+    const libraryType = 'dynamic';
+    const msvcVersions = ['v142', 'v143'];
+    const releaseFiles = ['libsodium.dll'];
+    const debugFiles = [...releaseFiles, 'libsodium.pdb'];
+    const configurations = {
+      'Debug': debugFiles,
+      'Release': releaseFiles,
+    };
+
+    for (final configEntry in configurations.entries) {
+      final config = configEntry.key;
+      final configFiles = configEntry.value;
+      for (final msvcVersion in msvcVersions) {
+        for (final file in configFiles) {
+          final source = extractDir
+              .subDir('libsodium')
+              .subDir(arch)
+              .subDir(config)
+              .subDir(msvcVersion)
+              .subDir(libraryType)
+              .subFile(file);
+          final target =
+              artifactDir.subDir(config).subDir(msvcVersion).subFile(file);
+          await target.parent.create(recursive: true);
+
+          GithubLogger.logNotice('Installing ${target.path}');
+          await source.rename(target.path);
+        }
+      }
+    }
   }
 }
