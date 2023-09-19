3cfa7a5164ccebab91e544faf6dfe61dbbddf17a
Skycoder42 <Skycoder42@users.noreply.github.com>
2023-09-19 08:25:46 +0200

    ...

--- packages/sodium_libs/tool/libsodium/platforms/linux_target.dart
+++ packages/sodium_libs/tool/libsodium/platforms/linux_target.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:dart_test_tools/tools.dart';
@@ -5,6 +6,8 @@ import 'package:dart_test_tools/tools.dart';
 import 'plugin_target.dart';
 
 class LinuxTarget extends PluginTarget {
+  static const _zigPublicKey = 'RWSGOq2NVecA2UPNdBUZykf1CCb147pkmdtYxgb3Ti';
+
   final String _architecture;
 
   const LinuxTarget({
@@ -22,10 +25,12 @@ class LinuxTarget extends PluginTarget {
     required Directory extractDir,
     required Directory artifactDir,
   }) async {
+    final zigPath = await _installZig();
+
     final buildDir = extractDir.subDir('libsodium-stable');
 
     await Github.exec(
-      'zig',
+      zigPath,
       [
         'build',
         '-Doptimize=ReleaseFast',
@@ -43,5 +48,33 @@ class LinuxTarget extends PluginTarget {
     Github.logInfo('Installing ${target.path}');
     await target.parent.create(recursive: true);
     await source.rename(target.path);
+  }
+
+  Future<String> _installZig() async {
+    final tmpDir = await Github.env.runnerTemp.createTemp();
+    final zigDir = await Github.env.runnerTemp.subDir('zig').create();
+    final client = HttpClient();
+    try {
+      final zigIndexRequest = await client
+          .getUrl(Uri.parse('https://ziglang.org/download/index.json'));
+      final zigIndexResponse = await zigIndexRequest.close();
+      final zigTarball = await zigIndexResponse
+          .transform(utf8.decoder)
+          .transform(json.decoder)
+          .cast<Map>()
+          .map((map) => map.entries.skip(1).first.value as Map)
+          .map((map) => map['x86_64-linux'] as Map)
+          .map((map) => map['tarball'] as String)
+          .single;
+
+      final zig = await client.download(tmpDir, Uri.parse(zigTarball));
+      await Minisign.verify(zig, _zigPublicKey);
+      await Archive.extract(archive: zig, outDir: zigDir);
+
+      return zigDir.subFile('zig').path;
+    } finally {
+      client.close();
+      await tmpDir.delete(recursive: true);
+    }
   }
 }
