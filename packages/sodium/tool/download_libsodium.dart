28075fe1a5cc16bd11d8e463e1c313ae02073e4a
Skycoder42 <Skycoder42@users.noreply.github.com>
2026-03-05 16:19:20 +0100

    ........

--- packages/sodium/tool/download_libsodium.dart
+++ packages/sodium/tool/download_libsodium.dart
@@ -3,13 +3,26 @@ import 'dart:io';
 import 'package:dart_test_tools/tools.dart';
 
 import '../../sodium_libs/libsodium_version.dart';
+import '../hook/build.dart';
 
 final _libsodiumDownloadUri = Uri.https(
   'download.libsodium.org',
   '/libsodium/releases/libsodium-${libsodium_version.ffi}-stable.tar.gz',
 );
 
-Future<void> main(List<String> args) => Github.runZoned(downloadLibsodium);
+Future<void> main(List<String> args) => Github.runZoned(() async {
+  await Github.logGroupAsync(
+    'Ensure minisign is installed',
+    Minisign.ensureInstalled,
+  );
+
+  await Github.logGroupAsync(
+    'Download, verify and extract libsodium sources',
+    downloadLibsodium,
+  );
+
+  await Github.env.setOutput(skipBuildHooksVariableName, '0', asEnv: true);
+});
 
 Future<void> downloadLibsodium() async {
   final downloadDir = Directory.fromUri(
